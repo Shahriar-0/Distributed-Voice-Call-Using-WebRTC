65fb14ee45e1c3cadaa4d489c3e2d3df9f29ab90
Shahriar-0 <attarshahriar@gmail.com>
2024-10-21 18:56:39 +0330

    adding q property

--- src/distributedlivevoicecall.cpp
+++ src/distributedlivevoicecall.cpp
@@ -1,18 +1,27 @@
 #include "distributedlivevoicecall.h"
 
 DistributedLiveVoiceCall::DistributedLiveVoiceCall(QObject *parent)
-    : QObject{parent}
+    : QObject{parent},
+      m_callerID("")  // Initialize the callerID to an empty string
 {}
 
-void DistributedLiveVoiceCall::startCall(const QString &callerID)
-{
-    // emit signal
-    // emit signalStartCall(callerID);
+// Getter for callerID
+QString DistributedLiveVoiceCall::callerID() const { return m_callerID; }
+
+// Setter for callerID
+void DistributedLiveVoiceCall::setCallerID(const QString &callerID) {
+    if (m_callerID != callerID) {
+        m_callerID = callerID;
+        emit callerIDChanged();  // Emit signal when callerID changes
+    }
+}
+
+void DistributedLiveVoiceCall::startCall(const QString &callerID) {
+    // Set the caller ID and emit the callerIDChanged signal if it has changed
+    setCallerID(callerID);
 
     // Debug print to ensure the method is called
     qDebug() << "Start call triggered with Caller ID:" << callerID;
 }
 
-void DistributedLiveVoiceCall::endCall() {
-    qDebug() << "End call triggered!";
-}
+void DistributedLiveVoiceCall::endCall() { qDebug() << "End call triggered!"; }
--- src/distributedlivevoicecall.h
+++ src/distributedlivevoicecall.h
@@ -6,14 +6,23 @@
 
 class DistributedLiveVoiceCall : public QObject {
     Q_OBJECT
+    Q_PROPERTY(QString callerID READ callerID WRITE setCallerID NOTIFY callerIDChanged)
+
 public:
     explicit DistributedLiveVoiceCall(QObject *parent = nullptr);
+
+    QString callerID() const;
+    void setCallerID(const QString &callerID);
 
 public slots:
     void startCall(const QString &callerID);
     void endCall();
 
+signals:
+    void callerIDChanged();
+
 private:
+    QString m_callerID;
 };
 
 #endif // DISTRIBUTEDLIVEVOICECALL_H
