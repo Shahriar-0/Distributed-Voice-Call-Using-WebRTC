5eb465f77a05a0dfe5952aaef7859e1c808a6128
Shahriar-0 <attarshahriar@gmail.com>
2024-10-24 20:55:34 +0330

    fuck u

--- src/distributedlivevoicecall.cpp
+++ src/distributedlivevoicecall.cpp
@@ -4,11 +4,9 @@
 DistributedLiveVoiceCall::DistributedLiveVoiceCall(QObject* parent)
     : QObject(parent), audioInput(nullptr), audioOutput(nullptr) {
 
-    // Initialize audio input and output objects
     audioInput = new AudioInput(this);
     audioOutput = new AudioOutput(this);
 
-    // Connect signal from AudioInput to process and immediately play encoded audio data
     connect(audioInput, &AudioInput::newAudioData, this, &DistributedLiveVoiceCall::handleNewAudioData);
 }
 
@@ -21,7 +19,6 @@ void DistributedLiveVoiceCall::startCall(const QString& callerID) {
     setCallerID(callerID);
     qDebug() << "Start call with Caller ID:" << callerID;
 
-    // Start audio recording and encoding
     if (!audioInput->startAudio()) {
         qWarning() << "Failed to start audio input.";
     }
@@ -30,7 +27,6 @@ void DistributedLiveVoiceCall::startCall(const QString& callerID) {
 void DistributedLiveVoiceCall::endCall() {
     qDebug() << "End call triggered!";
 
-    // Stop audio recording
     if (!audioInput->stopAudio()) {
         qWarning() << "Failed to stop audio input.";
     }
@@ -39,7 +35,6 @@ void DistributedLiveVoiceCall::endCall() {
 }
 
 void DistributedLiveVoiceCall::handleNewAudioData(const QByteArray &data) {
-    // Immediately decode and play the received Opus-encoded audio packet
     audioOutput->addData(data);
 
     qDebug() << "Playing new Opus packet of size:" << data.size();
