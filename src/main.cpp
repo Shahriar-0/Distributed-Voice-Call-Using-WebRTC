a66ab6852df3b83701fb8549b46de4014560ff3d
Matin Bazrafshan <matinbazrafshan2003@gmail.com>
2024-11-02 16:39:12 +0330

    test ss

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,62 @@
 #include <QGuiApplication>
 #include <QIcon>
 #include <QQmlApplicationEngine>
+#include <QTimer>
 
 #include "distributedlivevoicecall.h"
+#include "client.h"
+#include "signalingserver.h"
+#include <windows.h>
 
 int main(int argc, char *argv[]) {
-    QGuiApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
 
-    app.setWindowIcon(QIcon(":/assets/icon.png"));
+    // app.setWindowIcon(QIcon(":/assets/icon.png"));
     
-    QQmlApplicationEngine engine;
+    // QQmlApplicationEngine engine;
 
-    DistributedLiveVoiceCall* voiceCall = new DistributedLiveVoiceCall(&engine);
+    // DistributedLiveVoiceCall* voiceCall = new DistributedLiveVoiceCall(&engine);
 
-    qmlRegisterSingletonInstance("voiceCall", 1, 0, "DistributedLiveVoiceCall", voiceCall);
+    // qmlRegisterSingletonInstance("voiceCall", 1, 0, "DistributedLiveVoiceCall", voiceCall);
 
-    const QUrl url(QStringLiteral("qrc:/src/main.qml"));
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreated, &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl) QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
+    // const QUrl url(QStringLiteral("qrc:/src/main.qml"));
+    // QObject::connect(
+    //     &engine, &QQmlApplicationEngine::objectCreated, &app,
+    //     [url](QObject *obj, const QUrl &objUrl) {
+    //         if (!obj && url == objUrl) QCoreApplication::exit(-1);
+    //     },
+    //     Qt::QueuedConnection);
 
-    engine.load(url);
+    // engine.load(url);
 
-    if (engine.rootObjects().isEmpty()) return -1;
+    // if (engine.rootObjects().isEmpty()) return -1;
 
+    Client client1(nullptr, "client1");
+    Client client2(nullptr, "client2");
+    SignalingServer server;
+    if (!server.startServer(QHostAddress("127.0.0.1"), 9000)) {
+        qDebug() << "Failed to start server!";
+        return -1;
+    }
+
+
+    // client1.sendSdpToClient("sdddddp", "client2");
+    client1.connectToServer("127.0.0.1", 9000);
+    client2.connectToServer("127.0.0.1", 9000);
+
+
+
+    // while(1) {
+    //     if((client1.socket->state() == QAbstractSocket::ConnectedState)
+    //         && (client2.socket->state() == QAbstractSocket::ConnectedState))
+
+    //         break;   
+    // }
+
+    QTimer::singleShot(3000, [&] { client1.sendSdpToClient("sdddp", "client2"); });
+    qDebug() << "Here";
+    
+
+    
     return app.exec();
 }
